c7ff0fbcc95732c900156e53ff3d31f4d39a983d
JonatanFD <u20211b995@upc.edu.pe>
2026-04-18 19:45:34 -0500

    feat: add endpoints

--- src/services/solutions/controller.ts
+++ src/services/solutions/controller.ts
@@ -5,4 +5,16 @@ export class SolutionsController {
         const data = await http.get(`/solutions/challenge/${challengeId}`);
         return data.data;
     }
+
+    public static async updateSolution(solutionId: string, code: string) {
+        const data = await http.put(`/solutions/${solutionId}`, { code });
+        return data.data;
+    }
+
+    public static async submitSolution(solutionId: string, code: string) {
+        const data = await http.post(`/solutions/${solutionId}/submit`, {
+            code,
+        });
+        return data.data;
+    }
 }
